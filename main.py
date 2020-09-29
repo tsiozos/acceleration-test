8ff0afe4452246b2bf6dc91e2c6efd20ee3efa35
tsiozos <38419287+tsiozos@users.noreply.github.com>
2020-09-29 12:29:26 +0300

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ for i in range(100):
     sg /= 1024  #convert to g
     sg = sg * 9.81 - 9.81  # convert to m/sec^2 and subtract earth accel
     serial.write_string(str(input.running_time()/1000)+", "+str(sg)+serial.NEW_LINE)
-    basic.pause(1000)
+    #basic.pause(1000)
--- main.ts
+++ main.ts
@@ -12,5 +12,4 @@ for (let i = 0; i < 100; i++) {
     sg = sg * 9.81 - 9.81
     //  convert to m/sec^2 and subtract earth accel
     serial.writeString("" + input.runningTime() / 1000 + ", " + ("" + sg) + serial.NEW_LINE)
-    basic.pause(1000)
 }
