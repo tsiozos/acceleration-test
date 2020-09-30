0eaa8e80521245e256310dca3fa71431a2571af3
tsiozos <38419287+tsiozos@users.noreply.github.com>
2020-09-30 12:02:45 +0300

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ for i in range(100):
     sg = 0
     for j in range(100):
         sg += input.acceleration(Dimension.Strength)
-        control.wait_micros(10000) #wait 10 millis
+        control.wait_micros(5000) #wait 5 millis
     sg /= 100
     sg /= 1024  #convert to g
     sg = sg * 9.81  # convert to m/sec^2
-    serial.write_value("g (m/s^2)", Math.round_with_precision(sg,2))
-    basic.pause(1000)
+    serial.write_string(str(control.millis()//1000)+','+str(sg))
+    #basic.pause(1000)
