d50ec3ec46a36d9b2ddee45a57cf99809210dcdd
tsiozos <38419287+tsiozos@users.noreply.github.com>
2020-09-30 11:59:11 +0300

    Restore '9/29/2020, 11:31:29 AM Initial files for MakeCode project'

--- main.ts
+++ main.ts
@@ -3,13 +3,14 @@ for (let i = 0; i < 100; i++) {
     sg = 0
     for (let j = 0; j < 100; j++) {
         sg += input.acceleration(Dimension.Strength)
-        control.waitMicros(5000)
+        control.waitMicros(10000)
     }
-    // wait 5 millis
+    // wait 10 millis
     sg /= 100
     sg /= 1024
     // convert to g
     sg = sg * 9.81
     //  convert to m/sec^2
-    serial.writeString("" + Math.idiv(control.millis(), 1000) + "," + ("" + sg))
+    serial.writeValue("g (m/s^2)", Math.roundWithPrecision(sg, 2))
+    basic.pause(1000)
 }
